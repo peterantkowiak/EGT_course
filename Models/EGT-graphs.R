d6514ea56c67872a342c7f90673b255e0c1c745a
peterantkowiak <peter.antkowiak@gmx.net>
2014-12-11 11:43:03 +0100

    Signed-off-by: peterantkowiak <peter.antkowiak@gmx.net>

--- Models/EGT-graphs.R
+++ Models/EGT-graphs.R
@@ -4,33 +4,48 @@
 #---------------------------------------
 ########### read data #################
 
-
-#---------------------------------------
-########### calculate means ###########
-
-
-#---------------------------------------
-########### pot #######################
-
-
-exp <- read.csv("/home/Peter/Dokumente/uni/WS_14_15/Evolutionary Game Theory/r-scripts/PDstrategies.csv", head=T, skip=6, dec=".")
+exp <- read.csv("/home/Peter/Dokumente/uni/WS_14_15/Evolutionary Game Theory/EGT_course/Report/ResultsAndRcode/Task1_HD_nonspatial_nb8.csv", head=T, skip=6, dec=".")
 
 #install.packages("data.table")
 library(data.table)
 
+
 exp.s <- data.table(exp, key="X.run.number.")
-exp.s$ALLD_inv <- ifelse(exp.s$count.players.with..strategy....ALL_D.. == 0, FALSE, TRUE)
+#exp.s
 
-ALLD_levels <- matrix(nrow = length(unique(exp.s$initial_ALL_C)), ncol = 4, dimnames = list(as.character(unique(exp.s$initial_ALL_C)),c("initial_ALL_C","nr.TRUE","nr.FALSE","PropInv")))
-ALLD_levels[,1] <- unique(exp.s$initial_ALL_C)
+exp.s$r <- round(exp.s$cost/(2-exp.s$cost), digits = 3) 
+setnames(exp.s,8,"propC")
 
-for (i in 1:nrow(ALLD_levels)){
-  j <- ALLD_levels[i,1]
-  ALLD_levels[i,2] <- length(which((exp.s$initial_ALL_C == j) & (exp.s$ALLD_inv == TRUE)))
-  ALLD_levels[i,3] <- length(which((exp.s$initial_ALL_C == j) & (exp.s$ALLD_inv == FALSE)))
-  ALLD_levels[i,4] <- ALLD_levels[i,2] / (ALLD_levels[i,2] + ALLD_levels[i,3])
+r_levels <- matrix(nrow = length(unique(exp.s$r)), ncol = 4, dimnames = list(as.character(unique(exp.s$r)),c("r","mean","dev","serror")))
+r_levels[,1] <- unique(exp.s$r)
+
+
+
+#---------------------------------------
+########### calculate means ###########
+
+for (i in 1:nrow(r_levels)){
+  j <- r_levels[i,1]
+  rws <- subset(exp.s, r == j)
+  r_levels[i,2] <- mean(rws$propC)
+  r_levels[i,3] <- sd(rws$propC)
+  r_levels[i,4] <- sd(rws$propC)/sqrt(length(rws$propC))
+  rm(i,j,rws)
 }
 
-# View(ALLD_levels)
 
-plot(ALLD_levels[,1], ALLD_levels[,4], type = "l", xaxp = c(0, 0.45, 9), lwd=2, col="red", las=1, ylab="Proportion of runs with successful invasion of ALL_D  [ i = 20 ]", xlab="Initial proportion of ALL_C", main= "ALL_D invasion success in relation to initial ALL_C")
+#---------------------------------------
+########### plot ######################
+# install.packages("Hmisc")
+library(Hmisc)
+
+plot(r_levels[,1], r_levels[,2], type = "p", pch=19, col="red", las=1, ylab="Frequency of cooperation  [ t = 5000, i = 10 ]", xlab="cost / benefit ratio r", main="Task1_HD_nonspatial_nb8", )
+
+arrows(r_levels[,1], r_levels[,2]-1.96*r_levels[,4], r_levels[,1], r_levels[,2]+1.96*r_levels[,4], length=0.05, angle=90, code=3)
+# Error bars indicating the standard error with a 95 % confidence interval
+
+abline(1,-1,lty=2)
+
+
+
+
