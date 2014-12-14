71d9972bd24752eb84c717f1e737c4496b4536bb
peterantkowiak <peter.antkowiak@gmx.net>
2014-12-14 13:42:25 +0100

    Signed-off-by: peterantkowiak <peter.antkowiak@gmx.net>

--- Models/EGT-graphs.R
+++ Models/EGT-graphs.R
@@ -11,9 +11,9 @@ library(data.table)
 
 #filename <- "Task1_HD_nonspatial_nb8"
 #filename <- "Task1_HD_spatial_nb8"
-#filename <- "Task1_PD_nonspatial_nb8" #### corupt! Too low initial_propD!
+filename <- "Task1_PD_nonspatial_nb8" #### corupt! Too low initial_propD!
 #filename <- "Task1_PD_spatial_nb8"
-filename <- "Task2_HD_spatial_nb4"
+#filename <- "Task2_HD_spatial_nb4"
 #filename <- "Task2_HD_spatial_nb12"
 #filename <- "Task2_HD_spatial_nb24"
 #filename <- "Task2_PD_spatial_nb4"
