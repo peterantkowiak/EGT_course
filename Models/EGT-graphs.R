3801b8ed95517a435ecb9e1f059807c6180858d9
peterantkowiak <peter.antkowiak@gmx.net>
2014-12-16 12:50:01 +0100

    Signed-off-by: peterantkowiak <peter.antkowiak@gmx.net>

--- Models/EGT-graphs.R
+++ Models/EGT-graphs.R
@@ -6,12 +6,15 @@
 library(Hmisc)
 library(data.table)
 
+
+
+
 #---------------------------------------
 ########### read data #################
 
 #filename <- "Task1_HD_nonspatial"
 #filename <- "Task1_HD_spatial_nb8"
-#filename <- "Task1_PD_nonspatial" #### corupt! Too low initial_propD!
+#filename <- "Task1_PD_nonspatial" 
 #filename <- "Task1_PD_spatial_nb8"
 #filename <- "Task2_HD_spatial_nb4"
 #filename <- "Task2_HD_spatial_nb12"
@@ -20,17 +23,20 @@ library(data.table)
 #filename <- "Task2_PD_spatial_nb12"
 #filename <- "Task2_PD_spatial_nb24"
 #filename <- "Task3_HD_spatial_nb8_pure_10000"
-#filename <- "Task3_HD_spatial_nb8_mixed_10000_new"
-#filename <- "Task3_HD_spatial_nb8_mixed_10000_correct"
-filename <- "Task3_HD_spatial_nb8_mixed_10000_final"
+#filename <- "Task3_HD_spatial_nb8_mixed_10000_incorrect"
+#filename <- "Task3_HD_spatial_nb8_mixed_10000_final"
 
-### needs separate treatment --> in line "setnames", change ncol(exp) to ncol(exp)-1
-#filename <- "Task3_HD_spatial_nb8_mixed_10000"
+files <- c("Task1_HD_nonspatial","Task1_HD_spatial_nb8","Task1_PD_nonspatial","Task1_PD_spatial_nb8","Task2_HD_spatial_nb4","Task2_HD_spatial_nb12","Task2_HD_spatial_nb24","Task2_PD_spatial_nb4","Task2_PD_spatial_nb12","Task2_PD_spatial_nb24","Task3_HD_spatial_nb8_pure_10000","Task3_HD_spatial_nb8_mixed_10000_final")
 
-
+which <- c(1:4)
 
 directory <- "/home/Peter/Dokumente/uni/WS_14_15/Evolutionary Game Theory/EGT_course/Report/ResultsAndRcode/"
 
+autoplot <- function(files, which, directory){
+  filestoplot <-files[which]
+  for (h in filestoplot){
+
+filename <- h
 
 exp <- read.csv(paste0(directory,filename,".csv",collapse=""), , head=T, skip=6, dec=".")
 
@@ -66,14 +72,21 @@ for (i in 1:nrow(r_levels)){
 ########### plot ######################
 
 
-plot(r_levels[,1], r_levels[,3], type = "p", pch=19, col="red", las=1, ylab="Frequency of cooperation  [ t = 5000, i = 10 ]", xlab="cost / benefit ratio r", main=paste(filename), ylim = c(0,1))
+plot(r_levels[,1], r_levels[,3], type = "p", pch=19, col="red", las=1, ylab="Frequency of cooperation  [ t = 5000, i = 10 ]", xlab="cost / benefit ratio r", main=paste(substr(filename,7, nchar(filename))), ylim = c(0,1))
 grid()
 arrows(r_levels[,1], r_levels[,3]-r_levels[,5], r_levels[,1], r_levels[,3]+r_levels[,5], length=0.05, angle=90, code=3)
 # Error bars indicating the standard error with a 95 % confidence interval
 
-abline(1,-1,lty=2) # HD nonspatial pure and mixed
+if(substr(filename,7,8) == "HD") {
+  abline(1,-1,lty=2) }  # HD nonspatial pure and mixed
+if(substr(filename,7,8) == "PD") {
+  abline(0,0,lty=2) } # PD nonspatial pure
 
 #lines(r_levels[,1],1 - r_levels[,2]) # ???
 
+  }
+}
 
+par(mfrow=c(2,2))
 
+autoplot(files,which,directory)
